302fcb81c70248f82513981d894d3b5d68d33eab
jiebianng <897923551@qq.com>
2016-09-18 16:31:50 +0800

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,12 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
+        <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
+    <span>你倒萨大撒旦</span>
+    </div>
+  );
   }
 }
 
