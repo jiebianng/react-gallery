4b0a049b19a1382961fe5a033eb5a8c09668fdba
jiebianng <897923551@qq.com>
2016-09-20 13:28:45 +0800

    font

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,31 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let imageDatas = require('../data/imagesDatas.json');
 
+//利用自执行函数,讲图片名信息转成图片URL路径信息
+imageDatas = ((imageDatasArr) => {
+    for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    let singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+    }
+    return imageDatasArr;
+})(imageDatas);
 class AppComponent extends React.Component {
-  render() {
-    return (
-        <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-    <span>你倒萨大撒旦</span>
-    </div>
-  );
-  }
+    render() {
+        return (
+            <section className="stage">
+                <section className="img-sec">
+                </section>
+                <nav className="controller-nav">
+                </nav>
+            </section>
+        );
+    }
 }
-
 AppComponent.defaultProps = {
 };
 
